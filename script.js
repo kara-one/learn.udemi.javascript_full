f6a840fbc4fd2d0a3d43046d78220bf1de3811da
Serhii Karavashkin <ksawl@narod.ru>
2021-09-04 13:55:36 +0300

    lesson 2.12

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberOfFilms = prompt('Сколько фильмов уже просмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один из последних просмотенных фильмов?', ''),
+  b = prompt('На сколько оцените его?', ''),
+  c = prompt('Один из последних просмотенных фильмов?', ''),
+  d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
